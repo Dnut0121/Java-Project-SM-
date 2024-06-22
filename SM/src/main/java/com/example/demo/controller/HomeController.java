5300c71d1fe79446a1c9190932150aa4b961b048
Dnut0121 <127921103+Dnut0121@users.noreply.github.com>
2024-06-22 20:23:22 +0900

    SM

---
+++ SM/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/nike")
+    public String nike() {
+        return "nike";
+    }
+
+    @GetMapping("/adidas")
+    public String adidas() {
+        return "adidas";
+    }
+
+    @GetMapping("/converse")
+    public String converse() {
+        return "converse";
+    }
+
+    @GetMapping("/nb")
+    public String nb() {
+        return "nb";
+    }
+}
